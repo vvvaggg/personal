7fc80f235cdfc976863c0a7b4348c1db252b138f
vvvaggg <118966463+vvvaggg@users.noreply.github.com>
2023-04-21 15:09:11 +0300

    Add files via upload

--- vag1_2.cpp
+++ vag1_2.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 int main()
 {
@@ -6,15 +7,19 @@ int main()
     std::cin >> a;
 
     if (a>0)
+        {
         if (a%2==0)
             std::cout << "pologitelnoe chetnoe chislo" ;
         else
             std::cout << "pologitelnoe nechetnoe chislo" ;
+        }
     else if (a<0)
+        {
         if (a%2==0)
             std::cout << "otricatelnoe chetnoe chislo" ;
         else
             std::cout << "otricatelnoe nechetnoe chislo" ;
+        }
     else
         std::cout << "nulevoe chislo" ;
 
